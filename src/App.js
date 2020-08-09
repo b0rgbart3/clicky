8283efe58522d82eed72ba5ab699492972491c78
Bart Dority <b0rgBart3@gmail.com>
2020-08-09 11:34:28 -0700

    adding text to navbar and header with background image

--- src/App.js
+++ src/App.js
@@ -5,13 +5,29 @@ import Display from "./components/Display";
 let data=["1","2","3","4","5","6","7","8","9"];
 let chars=["bambam","barney","betty","boss","dino","dude","fred","gary","pebbles","pebbles2","perry","wilma"];
 function App(props) {
+
+//   let bigIBack = "/public/images/background_texture1.jpg";
+  let styles= {
+  myback: {
+    backgroundImage: `url(images/background_texture2.jpg)`,
+    backgroundColor: 'red'
+  }
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <div className="bigInstructions" style={styles.myback}>
+        <h1>Clicky Game!</h1>
+        <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+      </div>
       <Wrapper>
          <Display data={data} chars={chars} />
       </Wrapper>
+      <header className="App-header">
+        <div className="logotype">Clicky Game</div>
+        <div className="instructions">Click an image to begin!</div>
+        <div className="score">Score: 3  |   Top Score: 4</div>
+      </header>
     </div>
   );
 }
