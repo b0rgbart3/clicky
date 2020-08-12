376d07626889af9f93840fd5c2880b9163678fbf
Bart Dority <b0rgBart3@gmail.com>
2020-08-11 23:42:55 -0700

    Updates

---
+++ precache-manifest.840495e68c74df8ff7dff708e47ee2ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc1eaea4083b8ded4deeb06d7fe5219",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "9a1d2ae59fa92c4612df",
+    "url": "/clicky/static/css/main.c7d35a0b.chunk.css"
+  },
+  {
+    "revision": "04e2ab1f48986ce94fd1",
+    "url": "/clicky/static/js/2.29e08031.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/clicky/static/js/2.29e08031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1d2ae59fa92c4612df",
+    "url": "/clicky/static/js/main.2aef30c5.chunk.js"
+  },
+  {
+    "revision": "9868e024bc5f9d90297f",
+    "url": "/clicky/static/js/runtime-main.8db5a474.js"
+  }
+]);
