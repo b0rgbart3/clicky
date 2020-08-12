ac5f0e21a5d7e2f8931e7a5a2e75174c248e6511
Bart Dority <b0rgBart3@gmail.com>
2020-08-11 21:24:38 -0700

    changing data object to record clicks, and display correct message

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 function Card(props) {
 
   return (
-      <div className="card" onClick={() => { props.clickr(props.dataObject) } }>
+      <div className="card" onClick={() => { props.clickr(props.id) } }>
         <div className="img-container">
           <img alt={props.dataObject.name} src={"images/chars/" + props.dataObject.name + ".jpg"} />
         </div>
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,9 +1,10 @@
-import React from "react";
+
 import "./style.css";
 import Card from "../Card";
 import { Component } from "react";
 import data from "../../data.json";
 import { render } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
 
 // make a duplicate of the array
 let availableData = data.slice(0);
@@ -13,22 +14,56 @@ let topscore = 0;
 let sequence = [];
 let randomSequence = [];
 
+
+
+
 class Game extends Component {
+
   state = {
     data: data,
     sequence: sequence,
     score: score,
-    topscore: topscore
+    topscore: topscore,
+    success: "not_yet",
+    instruction: "Click an image to begin!"
   };
 
   componentDidMount() {
     this.chooseNextOrder();
   }
 
-clicked(dataObject) {
+clicked = id => {
+  // find the object that has this id in the original full data set
 
+    //let dataObject = this.state.data.filter(char => char.id === id)[0];
 
-console.log(dataObject);
+    // alternate method:
+
+    let dataObject = this.state.data[id-1];
+
+   // console.log("Found object: " + dataObject);
+ 
+    let newDataObject = [ dataObject ][0];
+    if (!dataObject.clicked) { 
+      newDataObject.clicked = true;
+      console.log(newDataObject);
+      this.state.score++;
+      console.log("Score: " + this.state.score);
+      this.state.success = "yes";
+      console.log("ID: " + dataObject.id);
+      this.state.data[dataObject.id-1] = newDataObject;
+      console.log(this.state.data);
+      this.state.instruction = "You guessed correctly!";
+      //console.log("Changing this to clicked: " + JSON.stringify(dataObject));
+    } else {
+      this.state.score = 0;
+      this.state.success = "no";
+      this.state.instruction = "You guessed incorrectly!";
+    }
+
+    this.setState({ data: this.state.data, score: this.state.score,
+    success: this.state.success  });
+
 
   }
 
@@ -52,22 +87,27 @@ chooseNextOrder() {
     // })
 }
 
+
 render() {
   return <div className="wrapper">
        {/* Pass the data array and the new random sequence to the Display */}
   
        {data.map( (char ,i) => {
-          console.log(char);
-            return <Card dataObject={char} key={ i } clickr={this.clicked}
-         />}
+        //  console.log(char);
+            return <Card dataObject={char} key={i} id={ char.id } 
+            clickr={this.clicked} />
+        }
           ) }
       
        <header className="App-header">
         <div className="logotype">Clicky Game</div>
-        <div className="instructions">Click an image to begin!</div>
-        <div className="score">Score: { score }  |   Top Score: { topscore }</div>
+
+        <div className={`instructions instructions-${this.state.success}`} 
+        
+        >{ this.state.instruction }</div>
+        <div className="score">Score: { this.state.score }  |   Top Score: { topscore }</div>
       </header>
-  </div>;
+  </div>
 }
 }
 
